6350929e6cb6e72bdb74fc987bf036209878e3d4
byhlui <brianyh.lui@gmail.com>
2021-05-01 15:14:09 -0400

    New notebook

---
+++ notebooks/Users/brianyh.lui@mail.utoronto.ca/Test Notebook.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Git Integration with Databricks
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Test Notebook
+# MAGIC See README.md on GitHub
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW DATABASES;
